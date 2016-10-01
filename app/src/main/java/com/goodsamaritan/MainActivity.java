f34fc5e19454fcfb9c723a6fc665971c9b33e01b
mayank-verma048 <mayank.verma048@gmail.com>
2016-10-01 14:39:30 +0530

    Initial commit

---
+++ app/src/main/java/com/goodsamaritan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.goodsamaritan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioButton male = (RadioButton) findViewById(R.id.radioMale);
+    }
+}
